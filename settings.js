c6f5d580005740b2e96c9dcb2fe2851023f23e43
Dulara Anjana <75074945+DularaAnana@users.noreply.github.com>
2022-10-05 15:34:25 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['94756408192'] //ur owner number
+global.ownername = "🎭𝙰𝙽𝙹𝙰𝙽𝙰" //ur owner name
+global.ytname = "YT: Dulara Anjana" //ur yt chanel name
+global.socialm = "Insta: dula_anj" //ur github or insta name
+global.location = "Srilanka,Matara." //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
+global.owner = ['94756408192']
+global.ownertag = '94763834336' //ur tag number
+global.botname = 'ᴡʙɢ ᴏꜰꜰɪᴄɪᴀʟ®️' //ur bot name
+global.linkz = "https://youtube.com/channel/UCqavu7x0voQw22mPAitZGPg" //your theme url which will be displayed on whatsapp
+global.websitex = "https://youtube.com/channel/UCqavu7x0voQw22mPAitZGPg" //ur website to be displayed
+global.botscript = 'https://youtube.com/channel/UCqavu7x0voQw22mPAitZGPg' //script link
 global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.themeemoji = "✴️" //ur theme emoji
+global.packname = "ᴡʙɢ ᴏꜰꜰɪᴄɪᴀʟ\n\n\n\n\n\n\n\n\n\n\n🔥 " //ur sticker watermark packname
+global.author = "🔥ᴡʙɢ" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
